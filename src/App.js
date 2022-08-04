29e63f6790c61a590aed1bb576b6be51a184dcd5
Usha Thapa (webQueen) <59187629+AyushaWorld@users.noreply.github.com>
2022-08-04 17:34:08 +0530

    product chnge
    react redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ProductListing from "./containers/ProductListing";
+import Home from "./containers/Home";
 import Header from "./containers/Header";
 import "./App.css";
 import ProductDetails from "./containers/ProductDetails";
@@ -11,7 +12,8 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route path="/" exact component={ProductListing} />
+          <Route path="/product" exact component={ProductListing} />
+          <Route path="/" exact component={Home} />
           <Route path="/product/:productId" component={ProductDetails} />
           <Route>404 Not Found!</Route>
         </Switch>
---
+++ src/containers/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../App.css'
+
+function App() {
+  return (
+    <div>
+     <div className="w-100 backImage" style={{height:"93vh"}}>
+     <h1 >Bettter digital experience with Presento</h1>
+        <h2>We are team of talented designers making websites with Bootstrap</h2> 
+        <button className="btn btn-success" type="submit">Get Started</button>
+     <div>
+        
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
